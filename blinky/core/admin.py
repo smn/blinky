bd71ef41224c3cf98f8878a80fd916a330518078
Simon de Haan <simon@praekelt.org>
2016-11-16 14:05:24 +0100

    tracking systems, worker types, worker instances, heartbeats

--- blinky/core/admin.py
+++ blinky/core/admin.py
@@ -11,16 +11,23 @@ class HeartBeatAdmin(admin.ModelAdmin):
 @admin.register(WorkerType)
 class WorkerAdmin(admin.ModelAdmin):
     list_filter = ('system',)
-    list_display = ('system', 'worker_name', 'created_at')
+    list_display = (
+        'system', 'worker_name', 'created_at', 'online', 'capacity',
+        'last_seen_at')
+
+    def last_seen_at(self, worker_type):
+        last_instance = worker_type.last_seen_instance
+        if last_instance:
+            return last_instance.last_seen_at
 
 
 @admin.register(WorkerInstance)
 class WorkerInstanceAdmin(admin.ModelAdmin):
     list_filter = ('worker_type',)
-    list_display = ('hostname', 'pid', 'worker_type')
+    list_display = ('hostname', 'pid', 'worker_type', 'online', 'last_seen_at')
 
 
 @admin.register(System)
 class SystemAdmin(admin.ModelAdmin):
     list_filter = ('created_at',)
-    list_display = ('system_id', 'created_at')
+    list_display = ('system_id', 'created_at', 'online')
--- blinky/core/models.py
+++ blinky/core/models.py
@@ -3,20 +3,85 @@ from __future__ import unicode_literals
 import pytz
 from django.db import models
 from datetime import datetime
+from django.utils import timezone
 
 
 class System(models.Model):
     system_id = models.CharField(max_length=255)
-    created_at = models.DateTimeField(auto_now_add=True, null=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    @property
+    def workertypes_online(self):
+        return [worker_type
+                for worker_type in self.workertype_set.filter(is_active=True)
+                if worker_type.online]
+
+    @property
+    def online(self):
+        return (
+            self.workertype_set.filter(is_active=True).exists() and
+            all(self.workertypes_online))
 
     def __unicode__(self):
         return self.system_id
 
 
 class WorkerType(models.Model):
+
+    DEFAULT_HEARTBEAT_INTERVAL = 10
+    CAPACITY_GOOD = 'CAPACITY_GOOD'
+    CAPACITY_OVER = 'CAPACITY_OVER'
+    CAPACITY_UNDER = 'CAPACITY_UNDER'
+
     system = models.ForeignKey(System)
     worker_name = models.CharField(max_length=255)
-    created_at = models.DateTimeField(auto_now_add=True, null=True)
+    heartbeat_interval = models.IntegerField(
+        default=DEFAULT_HEARTBEAT_INTERVAL)
+    created_at = models.DateTimeField(auto_now_add=True)
+    minimum_capacity = models.IntegerField(default=0)
+    maximum_capacity = models.IntegerField(default=10)
+    is_active = models.BooleanField(default=True)
+
+    @property
+    def last_seen_instance(self):
+        return max(self.workerinstance_set.all(),
+                   key=lambda instance: instance.last_seen_at)
+
+    @property
+    def instances_online(self):
+        return [worker_instance
+                for worker_instance in self.workerinstance_set.all()
+                if worker_instance.online]
+
+    @property
+    def instances_offline(self):
+        return [worker_instance
+                for worker_instance in self.workerinstance_set.all()
+                if not worker_instance.online]
+
+    @property
+    def online(self):
+        return any(self.instances_online)
+
+    @property
+    def capacity(self):
+        instance_count = len(self.instances_online)
+        if self.minimum_capacity <= instance_count <= self.maximum_capacity:
+            return self.CAPACITY_GOOD
+        elif instance_count < self.minimum_capacity:
+            return self.CAPACITY_UNDER
+        elif instance_count > self.maximum_capacity:
+            return self.CAPACITY_OVER
+
+    @classmethod
+    def garbage_collect(cls, gc_interval=60, now=None):
+        now = now or timezone.now()
+        for worker_type in WorkerType.objects.all():
+            last_instance = worker_type.last_seen_instance
+            delta = last_instance.last_seen_at - now
+            worker_type.is_active = (
+                abs(delta.total_seconds()) < float(gc_interval))
+            worker_type.save()
 
     def __unicode__(self):
         return self.worker_name
@@ -27,6 +92,16 @@ class WorkerInstance(models.Model):
     hostname = models.CharField(max_length=255)
     pid = models.IntegerField()
     created_at = models.DateTimeField(auto_now_add=True)
+
+    @property
+    def last_seen_at(self):
+        return self.heartbeat_set.latest().timestamp
+
+    @property
+    def online(self):
+        last_interval = (self.heartbeat_set.latest().timestamp -
+                         timezone.now()).total_seconds()
+        return abs(last_interval) < self.worker_type.heartbeat_interval
 
     def __unicode__(self):
         return u'%s:%s @ %s with pid %s' % (
@@ -39,9 +114,12 @@ class WorkerInstance(models.Model):
 class HeartBeat(models.Model):
     system = models.ForeignKey(System)
     worker_type = models.ForeignKey(WorkerType)
-    worker_instance = models.ForeignKey(WorkerInstance, null=True)
+    worker_instance = models.ForeignKey(WorkerInstance)
     timestamp = models.DateTimeField()
-    created_at = models.DateTimeField(auto_now_add=True, null=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    class Meta:
+        get_latest_by = 'timestamp'
 
     @classmethod
     def ingest(cls, data):

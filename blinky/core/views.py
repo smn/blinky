56b92960b1a3aa09968ac1dba5b18788ef4adb19
Simon de Haan <simon@praekelt.org>
2016-11-16 19:20:27 +0100

    fix filtering on heartbeats

--- blinky/core/views.py
+++ blinky/core/views.py
@@ -9,6 +9,7 @@ class WorkerTypeList(ListView):
 
 class WorkerInstanceList(ListView):
     model = WorkerInstance
+    ordering = '-created_at'
 
     def get_queryset(self):
         queryset = super(WorkerInstanceList, self).get_queryset()
@@ -26,6 +27,14 @@ class HeartBeatList(ListView):
     model = HeartBeat
     ordering = '-created_at'
     paginate_by = 30
+
+    def get_queryset(self):
+        queryset = super(HeartBeatList, self).get_queryset()
+        workertype = WorkerType.objects.get(pk=self.kwargs['workertype_pk'])
+        workerinstance = workertype.workerinstance_set.get(
+            pk=self.kwargs['workerinstance_pk'])
+        return queryset.filter(worker_type=workertype,
+                               worker_instance=workerinstance)
 
     def get_context_data(self, **kwargs):
         context = super(HeartBeatList, self).get_context_data(

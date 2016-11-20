469b6aed524c53947dcdccdeb7ff7e51800566a7
Simon de Haan <simon@praekelt.org>
2016-11-20 17:17:37 +0200

    quote_vhost and declare the queue were using

--- blinky/monitor/cli.py
+++ blinky/monitor/cli.py
@@ -2,18 +2,38 @@ import os
 import click
 import pika
 from pika.exceptions import ConnectionClosed
+from urlparse import urlparse, urlunparse
+from urllib import quote_plus
 import json
+import re
+
+
+def quote_vhost(broker_url):
+    """
+    NOTE:   The only reason this is needed is because the Pika library wants
+            the vhost section of the broker URL to be quoted so a leading
+            slash on a `/vhost` needs to be quoted as `'%2Fvhost`.
+    """
+    parse = urlparse(broker_url)
+    return urlunparse((
+        parse.scheme,
+        parse.netloc,
+        '/%s' % quote_plus(re.sub(r'^/', '', parse.path)),
+        parse.params,
+        parse.query,
+        parse.fragment
+    ))
 
 
 @click.command()
 @click.option('--broker-url',
-              default='amqp://guest:guest@localhost/', envvar='BROKER_URL')
+              default='amqp://guest:guest@localhost/', envvar='BROKER_URL',
+              type=quote_vhost)
 @click.option('--settings', default='blinky.settings')
 @click.option('--queue-name', default='heartbeat.inbound')
 @click.option('--exchange', default='vumi.health')
 @click.option('--verbose/--no-verbose', default=False)
 def run(broker_url, settings, queue_name, exchange, verbose):
-
     os.environ.setdefault('DJANGO_SETTINGS_MODULE', settings)
 
     import django
@@ -33,6 +53,7 @@ def run(broker_url, settings, queue_name, exchange, verbose):
     channel.exchange_declare(exchange=exchange,
                              type='direct',
                              durable=True)
+    channel.queue_declare(queue=queue_name, durable=True)
     channel.queue_bind(exchange=exchange,
                        queue=queue_name)
 
@@ -52,7 +73,3 @@ def run(broker_url, settings, queue_name, exchange, verbose):
         raise click.ClickException('Lost Connection to AMQP: %r' % e)
     except (KeyboardInterrupt,) as e:
         connection.close()
-
-
-if __name__ == '__main__':
-    run()

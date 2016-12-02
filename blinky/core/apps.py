fbd306b6f62e07b5da48b806bbf702b79d2920e3
Simon de Haan <simon@praekelt.org>
2016-12-02 16:04:46 +0100

    capacity change signal misfiring

--- blinky/core/apps.py
+++ blinky/core/apps.py
@@ -1,12 +1,13 @@
 from __future__ import unicode_literals
 
 from django.apps import AppConfig
-from django.db.models.signals import post_save
+# from django.db.models.signals import post_save
 
 
 class CoreAppConfig(AppConfig):
     name = 'blinky.core'
 
     def ready(self):
-        from .signal_callbacks import post_save_heartbeat
-        post_save.connect(post_save_heartbeat, sender='core.HeartBeat')
+        pass
+        # from .signal_callbacks import post_save_heartbeat
+        # post_save.connect(post_save_heartbeat, sender='core.HeartBeat')
